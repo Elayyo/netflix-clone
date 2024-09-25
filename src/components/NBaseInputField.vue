9db2f76cafbd945a047276148bf96a6fbf612e0a
Ayyoub <elayyo99@web.de>
2024-09-26 00:56:51 +0200

    refactored the input field to a seperate component

---
+++ src/components/NBaseInputField.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-72" :style="{'margin-top': mt+'rem','margin-bottom': mb+'rem'}">
+    <div class="relative w-full min-w-[200px] h-10">
+      <input
+          type="text"
+          class="peer w-full h-full bg-transparent text-white font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-gray-600 placeholder-shown:border-t-gray-600 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-gray-600 focus:border-white"
+          placeholder=" "
+      /><label
+        class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-gray-600 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-gray-600 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-600 peer-focus:text-white before:border-blue-gray-200 peer-focus:before:border-white after:border-gray-600 peer-focus:after:border-white"
+    >{{placeholder}}
+    </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  props: {
+    placeholder: {
+      type: String
+    },
+    "mt": {
+      type: Number
+    },
+    "mb": {
+      type: Number
+    }
+  }
+}
+</script>
+
--- src/views/NLogin.vue
+++ src/views/NLogin.vue
@@ -21,32 +21,8 @@
       >
         Einloggen
       </h2>
-<!--      maybe input feld und button als eigene komponente umso den code schlanker zu halten !!!!!!-->
-      <div class="w-72 my-4">
-        <div class="relative w-full min-w-[200px] h-10">
-          <input
-            type="email"
-            class="peer w-full h-full bg-transparent text-white font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-gray-600 placeholder-shown:border-t-gray-600 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-gray-600 focus:border-white"
-            placeholder=" "
-          /><label
-            class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-gray-600 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-gray-600 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-600 peer-focus:text-white before:border-blue-gray-200 peer-focus:before:border-white after:border-gray-600 peer-focus:after:border-white"
-            >Benutzername oder Email
-          </label>
-        </div>
-      </div>
-      <div class="w-72 mb-4">
-        <div class="relative w-full min-w-[200px] h-10">
-          <input
-            type="password"
-            class="peer w-full h-full bg-transparent text-white font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-gray-600 placeholder-shown:border-t-gray-600 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-gray-600 focus:border-white"
-            placeholder=" "
-          /><label
-            class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-gray-600 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-gray-600 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-600 peer-focus:text-white before:border-blue-gray-200 peer-focus:before:border-white after:border-gray-600 peer-focus:after:border-white"
-            >Passwort
-          </label>
-
-        </div>
-      </div>
+      <n-base-input-field :placeholder="'Email oder Username'" :mt="1" :mb="1"></n-base-input-field>
+      <n-base-input-field :placeholder="'Passwort'" :mb="1"></n-base-input-field>
       <button
         type="button"
         data-ripple-light="true"
@@ -65,9 +41,11 @@
 
 <script>
 import NRegistration from "@/components/NRegistration.vue";
+import NBaseInputField from "@/components/NBaseInputField.vue";
 export default {
   components: {
-    NRegistration
+    NRegistration,
+    NBaseInputField,
   },
   data: function () {
     return {
